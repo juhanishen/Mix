0b98618b464a8f43010c2b811745310ac0795960
juhanishen <juhanishen@gmail.com>
2014-11-10 14:54:51 +0200

    add enhanced Fibonacci algorithm
    add cache to calculated element

--- src/main/java/org/juhani/fib/FibMain.java
+++ src/main/java/org/juhani/fib/FibMain.java
@@ -1,14 +1,28 @@
 package org.juhani.fib;
 
+import java.util.HashMap;
+
 
 public class FibMain{
 
+	private static HashMap<Integer,Integer> fibCache = new HashMap<>();
+	
     public static void main(String[] args){
+       int n=40;	
     	
-       long start = System.nanoTime();	
-	   System.out.println("sum is:"+fib(40));
-	   double duration = ((double) (System.nanoTime()-start)) /1000/1000;
-	   System.out.println("duration is (milli seconds): "+duration);
+       long startNormal = System.nanoTime();	
+       int normalAlgorithm = fib(n);
+	   System.out.println("sum of normal algorithm is:"+normalAlgorithm);
+	   double durationNormal = ((double) (System.nanoTime()-startNormal)) /1000/1000;
+	   System.out.println("normal algorithm duration is (milli seconds): "+durationNormal);
+	   
+	   long startEnhanced = System.nanoTime();	
+       int enhancedAlgorithm = fibEnhanced(n);
+	   System.out.println("sum of enhanced algorithm is:"+enhancedAlgorithm);
+	   double durationEnhanced = ((double) (System.nanoTime()-startEnhanced)) /1000/1000;
+	   System.out.println("enhanced algorithm duration is (milli seconds): "+durationEnhanced);
+       
+	   assert(normalAlgorithm == enhancedAlgorithm);
     }
     
     public static int fib(int n){    	
@@ -20,5 +34,29 @@ public class FibMain{
         return fib(n-1)+fib(n-2);
     }
     
+    
+    public static int fibEnhanced(int n){
+    	if(n==0){
+    		fibCache.put(0,1);
+    		return 1;
+    	}else if(n==1){
+    		fibCache.put(1,1);
+    		return 1;
+    	}
+    	if(fibCache.keySet().contains(n-2) && fibCache.keySet().contains(n-1)){
+    		int sum = fibCache.get(n-2)+fibCache.get(n-1);
+    		fibCache.put(n,sum);
+    		return sum;
+    	}else if(fibCache.keySet().contains(n-2) && (!fibCache.keySet().contains(n-1))){
+    		int sum = fibCache.get(n-2)+fibEnhanced(n-1);
+    		fibCache.put(n,sum);
+    		return sum;     		
+    	}else if(!fibCache.keySet().contains(n-2)){
+    		int sum = fibEnhanced(n-2)+fibEnhanced(n-1);
+    		fibCache.put(n,sum);
+    		return sum;     		
+    	}
+    	return -1;
+    }
 }
     
