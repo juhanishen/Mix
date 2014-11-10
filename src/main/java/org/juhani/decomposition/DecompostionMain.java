72aeaac95a4949a00a46e3d1bf389f30bc44583f
juhanishen <juhanishen@gmail.com>
2014-11-10 15:09:29 +0200

    add decompostion algo. description
    add decompostion algo. description or problem domain

--- src/main/java/org/juhani/decomposition/DecompostionMain.java
+++ src/main/java/org/juhani/decomposition/DecompostionMain.java
@@ -1,5 +1,29 @@
+/**
+ * Given an Integer, for example, 11. A decompostion means sum of integer equal to the given
+ * number. For example, give by 11, a couple of decompostion is as follows:
+ *  11 = 1+10;
+ *  11 = 11;
+ *  11= 1+3+7;
+ *  11=2+8+1;
+ *  11 = 5+6;
+ *    …
+ * An order decompostion means all integer which sums equal to given number is odd number. 
+ * For example, 
+ *  11 = 1+3+7
+ *  11 = 11;
+ *  is two odd decompostion. The maximum number of integer is called maixmum odd decompostion.
+ *  For example, 11=1+3+7 is maximum odd decompostion since it has 3 integers. 
+ *  You are suppose to write a program as follows public int[] solution(int N), to return the 
+ *  maximum odd decompostion. There could be several possible maximum odd decompostion, one only
+ *  need to return one of them.
+ */
+
 package org.juhani.decomposition;
 
 public class DecompostionMain {
+	
+	public static void main(){
+		
+	}
 
 }
